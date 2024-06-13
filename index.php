55ac7eea31a016f2a9515de1a9948d04989a7a07
FBD <fbd@Air-de-FBD.lan>
2024-06-13 22:16:29 +0200

    init projet

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php require_once __DIR__. '/templates/header.php' ?>
+
+
+        <section class="container col-xxl-8 px-4 py-5">
+            <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+                <div class="col-10 col-sm-8 col-lg-6">
+                    <img src="./assets/images/logo-cuisinea.jpg" class="d-block mx-lg-auto img-fluid" alt="logo" width="350">
+                </div>
+                <div class="col-lg-6">
+                    <h1 class="fw-bold lh-1 mb-3">Responsive left-aligned hero with image</h1>
+                    <p>Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+                    <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                    <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="container text-center">
+            <article class="row text-center">
+                <div class="col-md-4"> 
+                    <div class="card">
+                        <img src="./uploads/1-chocolate-au-mousse.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4"> 
+                    <div class="card">
+                        <img src="./uploads/2-gratin-dauphinois.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4"> 
+                    <div class="card">
+                        <img src="./uploads/3-salade.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </article>
+        </section>
+
+
+
+<?php require_once __DIR__. '/templates/footer.php' ?>  
