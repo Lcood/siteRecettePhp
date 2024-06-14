dd11a79c1bd787932a5655fb46f5eb56f8375d9f
FBD <fbd@Air-de-FBD.lan>
2024-06-14 22:11:22 +0200

    = connexion

--- recette.php
+++ recette.php
@@ -3,9 +3,14 @@
 require_once __DIR__. '/templates/header.php';
 require_once ('./lib/recipe.php');
 
-$id = $_GET['id'];
+$pdo = new PDO('mysql:host=localhost;dbname=siteRecette;charset=utf8', 'root', 'root');
 
-//var_dump($recipes[$id]);
+$id = $_GET['id'];
+$query = $pdo->prepare("SELECT * FROM recipes WHERE id = :id");
+$query->bindParam(':id', $id, PDO::PARAM_INT);
+$query->execute();
+$recipe = $query->fetch();
+
 
 ?>
 
@@ -13,11 +18,11 @@ $id = $_GET['id'];
     <section class="container col-xxl-8 px-4 py-5">
                 <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
                     <div class="col-10 col-sm-8 col-lg-6">
-                    <img src="<?=_RECIPES_IMG_PATH_.$recipes[$id]['image']; ?>" class="d-block mx-lg-auto img-fluid" alt="image_recette" width="350">
+                    <img src="<?=_RECIPES_IMG_PATH_.$recipe['image']; ?>" class="d-block mx-lg-auto img-fluid" alt="image_recette" width="350">
                     </div>
                     <div class="col-lg-6">
-                        <h1 class="fw-bold lh-1 mb-3"><?=$recipes[$id]['title'];?></h1>
-                        <p><?=$recipes[$id]['description'];?></p>
+                        <h1 class="fw-bold lh-1 mb-3"><?=$recipe['title'];?></h1>
+                        <p><?=$recipe['description'];?></p>
                         <div class="d-grid gap-2 d-md-flex justify-content-md-start">
                         </div>
                     </div>
